456f7464ba5572dc9d919f28782b320d1157429b
michel-a <malvesdesign@hotmail.com>
2020-02-23 17:32:27 -0300

    Bad solution

--- src/application/Aula138.java
+++ src/application/Aula138.java
@@ -37,15 +37,14 @@ public class Aula138 {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates.");
-			} else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-ou date must be after check-In date.");
-			} else {
-				reservation.updateDates(checkIn, checkOut);
+			String error = reservation.updateDates(checkIn, checkOut);
+			
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
+			} else { 
 				System.out.println("Reservation: " + reservation);
 			}
+			
 		}
 		
 		sc.close();
